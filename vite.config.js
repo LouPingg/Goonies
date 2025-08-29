4bb18bf819e24a368fc99655692b40dd0d97db15
Luxiee <lechatdesboisbois@hotmail.com>
2025-08-30 00:54:06 +0200

    builde page + api url prod

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Goonies/",
+  base: "/Goonies/", // ⚠️ remplace par le nom exact de ton repo GitHub
+  build: { outDir: "docs" }, // pour publier via /docs
 });
